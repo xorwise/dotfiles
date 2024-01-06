d0b7c0f2651b3a831da41987b6d475a13b9a993f
xorwise <xorwise@gmail.com>
2024-01-06 23:01:43 +0300

    fix: config

--- .config/nvim/lua/plugin/lsp.lua
+++ .config/nvim/lua/plugin/lsp.lua
@@ -1,76 +1,71 @@
 return {
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip",
     {
-        {
-            "williamboman/mason-lspconfig.nvim",
-            dependencies = {
-                "williamboman/mason.nvim",
-            },
-            config = function()
-                require("mason").setup({})
-                require("mason-lspconfig").setup({
-                    ensure_installed = {
-                        "pyright",
-                        "clangd",
-                        "cssls",
-                        "eslint",
-                        "html",
-                        "tsserver",
-                    },
-                    automatic_installation = true,
-                    handlers = {
-                        function(server_name) -- default handler (optional)
-                            require("lspconfig")[server_name].setup {}
-                        end,
-                    },
-                })
-            end,
-        },
-        {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
             "neovim/nvim-lspconfig",
-            config = function()
-                require("lspconfig").lua_ls.setup({
-                    settings = {
-                        Lua = {
-                            diagnostics = {
-                                globals = { "vim" },
-                            },
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            require("mason").setup({})
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "pyright",
+                    "clangd",
+                    "cssls",
+                    "eslint",
+                    "html",
+                    "tsserver",
+                },
+                automatic_installation = true,
+                handlers = {
+                    function(server_name) -- default handler (optional)
+                        require("lspconfig")[server_name].setup {}
+                    end,
+                },
+            })
+            require("lspconfig").lua_ls.setup({
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
                         },
                     },
-                })
-                require("lspconfig").pyright.setup({
-                    settings = {
-                        python = {
-                            analysis = {
-                                typeCheckingMode = "off",
-                            },
+                },
+            })
+            require("lspconfig").pyright.setup({
+                settings = {
+                    python = {
+                        analysis = {
+                            typeCheckingMode = "off",
                         },
                     },
+                },
+            })
+            local cmp = require('cmp')
+
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    -- `Enter` key to confirm completion
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+                    -- Ctrl+Space to trigger completion menu
+                    ['<C-Space>'] = cmp.mapping.complete(),
+
+                    -- Scroll up and down in the completion documentation
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' }, -- For luasnip users.
+                }, {
+                    { name = 'buffer' },
                 })
-            end
-        },
-        { "hrsh7th/cmp-nvim-lsp" },
-
-        {
-            "hrsh7th/nvim-cmp",
-            config = function()
-                local cmp = require('cmp')
-
-                cmp.setup({
-                    mapping = cmp.mapping.preset.insert({
-                        -- `Enter` key to confirm completion
-                        ['<CR>'] = cmp.mapping.confirm({ select = false }),
-
-                        -- Ctrl+Space to trigger completion menu
-                        ['<C-Space>'] = cmp.mapping.complete(),
-
-                        -- Scroll up and down in the completion documentation
-                        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    })
-                })
-            end
-        },
-        { "L3MON4D3/LuaSnip" },
+            })
+        end,
     },
     {
         "ray-x/lsp_signature.nvim",
