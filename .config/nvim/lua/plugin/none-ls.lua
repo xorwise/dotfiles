436581d29ea4a2c998b0f92c7263a01eed337694
xorwise <xorwise@gmail.com>
2024-01-07 14:53:28 +0300

    fix: changed null_ls to none_ls

--- .config/nvim/lua/plugin/none-ls.lua
+++ .config/nvim/lua/plugin/none-ls.lua
@@ -1,5 +1,5 @@
 return {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     config = function()
         local null_ls = require("null-ls")
         local formatting = null_ls.builtins.formatting
@@ -10,7 +10,7 @@ return {
             sources = {
                 -- formatting
                 formatting.stylua,
-                formatting.yapf,
+                formatting.black,
                 -- diagnostics
                 diagnostics.mypy.with({
                     extra_args = function()
