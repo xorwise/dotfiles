2bbecdfeb09e4dc7645cb495ef83579748defbe6
xorwise <xorwise@gmail.com>
2024-04-11 01:17:33 +0300

    change autocompletion bindings

--- .config/nvim/lua/plugin/lsp.lua
+++ .config/nvim/lua/plugin/lsp.lua
@@ -55,10 +55,11 @@ return {
 
                     -- Ctrl+Space to trigger completion menu
                     ["<C-Space>"] = cmp.mapping.complete(),
-
                     -- Scroll up and down in the completion documentation
                     ["<C-u>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                    ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+                    ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
