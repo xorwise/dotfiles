c3fab22b7021456a38d37a948bb2e00122d76dd2
xorwise <xorwise@gmail.com>
2024-04-11 22:52:06 +0300

    added hidden files into telescope find_files

--- .config/nvim/lua/plugin/telescope.lua
+++ .config/nvim/lua/plugin/telescope.lua
@@ -4,18 +4,6 @@ return {
     -- or                              , branch = '0.1.x',
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>fa", builtin.buffers, {})
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-        vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "[G]oto [R]eferences" })
-        vim.keymap.set('n', '/', function()
-            builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
-                previewer = false,
-            }))
-        end)
         require("telescope").setup({
             defaults = {
                 file_ignore_patterns = {
@@ -28,6 +16,20 @@ return {
                 },
             },
         })
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", function()
+            builtin.find_files({ hidden = true })
+        end, {})
+        vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>fa", builtin.buffers, {})
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+        vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, { desc = "[G]oto [R]eferences" })
+        vim.keymap.set('n', '/', function()
+            builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
+                previewer = false,
+            }))
+        end)
 
         require("telescope").load_extension("git_worktree")
         vim.keymap.set("n", "<leader>wt", function()
