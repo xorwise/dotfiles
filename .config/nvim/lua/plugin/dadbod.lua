b2cdf14709515b1ab43d838c55dce7f64173b731
xorwise <xorwise@gmail.com>
2024-09-25 15:02:44 +0300

    added keymap for dadbod

--- .config/nvim/lua/plugin/dadbod.lua
+++ .config/nvim/lua/plugin/dadbod.lua
@@ -1,5 +1,10 @@
 return {
     "tpope/vim-dadbod",
     "kristijanhusak/vim-dadbod-completion",
-    "kristijanhusak/vim-dadbod-ui",
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        config = function()
+            vim.keymap.set("n", "<leader>db", ":DBUIToggle<CR>", { noremap = true, silent = true })
+        end,
+    },
 }
